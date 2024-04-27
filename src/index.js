c1486085cd4f1450231fd61ad20789f0875021d1
prabhakarsingh1030 <119883350+prabhakarsingh1030@users.noreply.github.com>
2024-04-27 12:22:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <ul>
+      <li>car</li>
+      <li>bike</li>
+      <li>motor</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
